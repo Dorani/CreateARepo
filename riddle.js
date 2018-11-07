d0146d3e44fc629fc54622113138b7778b44196d
Lol Cinnamonbuns <sebastiansryan@gmail.com>
2018-11-06 18:53:47 -0800

    added riddle

---
+++ riddle.js
@@ -0,0 +1,3 @@
+var riddle = "if a baby's born at 1:59am and then its twin is born 2 minutes later but the clocks went back so now it's 1:01am, which is older because the one born second will be older on paper but the one born first i actually 2 minutes older???";
+
+console.log(riddle);
